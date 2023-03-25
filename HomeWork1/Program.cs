e18fa0bb14db3249bce9859eee75d7e1f70e349b
Anna <morozova.anna.2014year@gmail.com>
2023-03-25 18:06:45 +0300

    Done

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,1 +1,65 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+//     {
+//         Console.Write($"Число {num1} больше числа {num2}");
+//     }
+// else
+//     {
+//         Console.WriteLine($"Число {num2} больше числа {num1}");
+//     }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2 & num1 > num3)
+//     {
+//         Console.WriteLine($"Максимальное число {num1}");
+//     }
+// else if (num2 > num1 & num2 > num3)
+//     {
+//         Console.WriteLine($"Максимальное число {num2}");
+//     }
+// else
+//     {
+//         Console.WriteLine($"Максимальное число {num3}");
+//     }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+//     {
+//         Console.WriteLine("Число четное");
+//     }
+// else
+//     {
+//         Console.WriteLine("Число нечетное");
+//     }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// for (int a = 2; a <= num; a = a + 2)
+//     {
+//         Console.WriteLine(a);
+//     }
