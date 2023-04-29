d1ee71ad430cf8520c0abba141621cf1d2bea30f
Anna <morozova.anna.2014year@gmail.com>
2023-04-29 16:47:20 +0300

    Done

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -223,10 +223,6 @@
 
 
 
-
-
-
-
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
@@ -235,59 +231,43 @@
 // 26(1,0,1) 55(1,1,1)
 
 
+// void ShowIndex(int[,,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.WriteLine();
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+//             }
+//         }
+//     }
+// }
 
+// void CreateArray3D(int[,,] array)
+// {
+//     int count = 10;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 array[k, i, j] += count;
 
+//                int num = new Random().Next(1, 10);
+//                count = count + num;
+//             }
+//         }
+//     }
+// }
 
-// Функция вывода индекса элементов 3D массива
-void ShowIndex(int[,,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.WriteLine();
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
-            }
-        }
-    }
-}
-
-// Функция заполнения 3D массива не повторяющимеся числами
-void CreateArray3D(int[,,] array)
-{
-    int count = 10;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                array[k, i, j] += count;
-
-               int num = new Random().Next(1, 10);
-               count = count + num;
-            }
-        }
-    }
-}
-
-int[,,] array3D = new int[2, 2, 2];
-CreateArray3D(array3D);
-ShowIndex(array3D);
+// int[,,] array3D = new int[2, 2, 2];
+// CreateArray3D(array3D);
+// ShowIndex(array3D);
 
 
 
 
-
-
-
-
-
-// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
-// Например, на выходе получается вот такой массив:
-// 01 02 03 04
-// 12 13 14 05
-// 11 16 15 06
-// 10 09 08 07
